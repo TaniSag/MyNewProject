c7c218c1f6f9c9efba78fef079dfb6112a2962e7
TaniS <t6346616@gmail.com>
2020-10-06 14:31:50 +0300

    New1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World Sunny day");
+        System.out.println();
+        System.out.println("Hello again");
+        System.out.println("Line 1\n\tLine 2\n\t\tLine 3");
+        System.out.println("Fin");
+    }
+}
